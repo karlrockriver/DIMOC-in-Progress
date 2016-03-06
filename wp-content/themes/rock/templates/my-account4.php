ba858e5db7412489e0f5b6d20e0c462fb32c9eee
Karl Larson <KarlLarson@ksl.local>
2016-03-05 17:04:08 -0700

    My Account 3 & 4 Added
    Responsive tables added to My Account 3 & 4;
    My Account 1, 2, 3, and 4 need to be merged into one page.

---
+++ wp-content/themes/rock/templates/my-account4.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Template Name: My Account 4
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Rock
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		
+<div class="area-white">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-lg-3">
+				<div class="sign-in-box">
+					<h1 class="text-color-2"><?php the_field('account4_header'); ?></h1>
+					<a href="#"><button class="button-flat-full-color-1" style="margin-top: 0">Status</button></a>
+					<a href="#"><button class="button-flat-full-color-1" style="margin-top: 0">Download History</button></a>
+					<a href="#"><button class="button-flat-full-color-1" style="margin-top: 0">Lightboxes</button></a>
+					<a href="#"><button class="button-flat-full-color-1" style="margin-top: 0">Edit Profile</button></a>
+				</div>
+			</div>
+
+			<div class="col-xs-12 col-lg-9">
+				<div class="row">
+					<div class="col-xs-12">
+						<table class="table-2">
+							<thead>
+							<tr>
+								<th>File</th>
+								<th>ID</th>
+								<th>Date</th>
+								<th>Fees</th>
+								<th>License</th>
+								<th>Size</th>
+								<th>Type</th>
+								<th>Download</th>
+							</tr>
+							</thead>
+							<tbody>
+							<tr>
+								<td><img src="#"></td>
+								<td>Resource/Name</td>
+								<td>DD/MM/YYYY</td>
+								<td>Subscription</td>
+								<td><a href="#">Standard</a></td>
+								<td>Format FileSize</td>
+								<td>Photo/Video</td>
+								<td><a href="#">Download</a></td>
+							</tr>
+							<tr>
+								<td><img src="#"></td>
+								<td>Resource/Name</td>
+								<td>DD/MM/YYYY</td>
+								<td>Subscription</td>
+								<td><a href="#">Standard</a></td>
+								<td>Format FileSize</td>
+								<td>Photo/Video</td>
+								<td><a href="#">Download</a></td>
+							</tr>
+							<tr>
+								<td><img src="#"></td>
+								<td>Resource/Name</td>
+								<td>DD/MM/YYYY</td>
+								<td>Subscription</td>
+								<td><a href="#">Standard</a></td>
+								<td>Format FileSize</td>
+								<td>Photo/Video</td>
+								<td><a href="#">Download</a></td>
+							</tr>
+							
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+
+get_footer();
